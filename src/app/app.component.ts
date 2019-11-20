378f0191d807c3682d33fa9e57a937d4a3ddeff9
hurrybote <ganmarumu0509@gmail.com>
2019-11-20 16:16:38 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { literal } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'PokerAnalyzor';
+  name = '';
+  result = '';
+
+  constructor(private http: HttpClient){}
+
+  onchange(list: any){
+    if(list.length <= 0){return;}
+
+    let f = list[0];
+    let data = new FormData();
+    data.append('upfile', f, f.name);
+
+    
+  }
 }
