7ed5f8db50fb0a59b787e6e0458171d5077979af
hurrybote <hurrybote@common-no-iMac-2.local>
2019-11-25 22:07:16 +0900

    update

--- src/app/odds-simulation/odds-simulation.component.ts
+++ src/app/odds-simulation/odds-simulation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { HAND_PAIR } from './hand-pair';
+import { HAND_PAIR, HandPair } from './hand-pair';
 
 @Component({
   selector: 'app-odds-simulation',
@@ -8,6 +8,7 @@ import { HAND_PAIR } from './hand-pair';
   encapsulation: ViewEncapsulation.None
 })
 export class OddsSimulationComponent implements OnInit {
+  private hand_pair: HandPair[] = HAND_PAIR;
 
   constructor() { }
 
@@ -15,8 +16,16 @@ export class OddsSimulationComponent implements OnInit {
   }
 
   formatLabel(value: number) {
-    return value + '%'
+    for(let i=0; i <= this.hand_pair.length; i++){
+      if (this.hand_pair[i]["p1"] >= 100-value) {
+        this.hand_pair[i]["p1"] = 1;
+      }else{
+        this.hand_pair[i]["p1"] = 0;
+      }
+    }
+    return value + '%';
   }
 
-  public hand_pair = HAND_PAIR;
+
+  
 }
